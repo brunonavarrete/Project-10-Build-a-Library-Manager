74f3fcb65046dab1033a332e3af24e5683d5a2f5
brunouno <bruno.navarreteon@gmail.com>
2017-07-29 00:17:31 -0500

    server, initial route / and initial pug files (layout, header, index)

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
+const app = express();
+
+// middleware
+	app.use( bodyParser.urlencoded({extended:false}) );
+	app.use( cookieParser() );
+	app.use('/static', express.static(__dirname + '/public'));
+
+// pug
+	app.set('view engine', 'pug');
+
+// routes
+	const mainRoutes = require('./routes');
+	// const cardRoutes = require('./routes/cards');
+
+	app.use(mainRoutes);
+	// app.use('/cards',cardRoutes);
+
+// ERRORS
+	app.use((req,res,next) => {
+		const err = new Error('Not Found');
+		err.status = 404;
+		next(err);
+	});
+
+	app.use((err,req,res,next) => {
+		res.locals.err = err;
+		res.status(err.status);
+		res.render('error',err);
+	});
+
+//server
+	app.listen(3000, () => {
+		console.log('The application is running on localhost:3000');
+	});
