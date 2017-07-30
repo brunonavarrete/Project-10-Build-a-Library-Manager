62527b6e7052ee64d7f518f3dfbcdf05399c1867
brunouno <bruno.navarreteon@gmail.com>
2017-07-30 09:14:55 -0500

    Loan[i].Patron to book_detail

--- routes/books.js
+++ routes/books.js
@@ -5,71 +5,77 @@ const Patron = require('../models').Patron;
 const Loan = require('../models').Loan;
 
 // relations
-Loan.belongsTo(Book);
-Book.hasMany(Loan);
-	// books
-		// GET
-			router.get('', (req, res) => {
-				res.redirect('/books/all');
-			});
+	Loan.belongsTo(Book);
+	Book.hasMany(Loan);
+	Loan.belongsTo(Patron);
+	
+// GET
+	router.get('', (req, res) => {
+		res.redirect('/books/all');
+	});
 
-			router.get('/all', (req, res) => {
-				Book.findAll().then( (books) => {
-					res.render('all_books',{books});
-				});
-			});
+	router.get('/all', (req, res) => {
+		Book.findAll().then( (books) => {
+			res.render('all_books',{books});
+		});
+	});
 
-			router.get('/new', (req, res) => {
-				res.render('new_book');
-			});
+	router.get('/new', (req, res) => {
+		res.render('new_book');
+	});
 
-			router.get('/checked', (req, res) => {
-				Loan.findAll({
-					where: { 
-						returned_on:null,
-					},
-					include: [Book],
-				}).then( (loans) => {
-					res.render('overdue_books',{loans,title:'Checked Out Books'});
-				});
-			});
+	router.get('/checked', (req, res) => {
+		Loan.findAll({
+			where: { 
+				returned_on:null,
+			},
+			include: [Book],
+		}).then( (loans) => {
+			res.render('overdue_books',{loans,title:'Checked Out Books'});
+		});
+	});
 
-			router.get('/overdue', (req, res) => {
-				Loan.findAll({
-					where: { 
-						returned_on:null,
-						return_by: {
-							$lt: new Date()
-						}
-					},
-					include: [Book],
-				}).then( (loans) => {
-					res.render('overdue_books',{loans,title:'Overdue Books'});
-				});
-			});
+	router.get('/overdue', (req, res) => {
+		Loan.findAll({
+			where: { 
+				returned_on:null,
+				return_by: {
+					$lt: new Date()
+				}
+			},
+			include: [Book],
+		}).then( (loans) => {
+			res.render('overdue_books',{loans,title:'Overdue Books'});
+		});
+	});
 
-			router.get('/:id', (req, res) => {
-				Book.find({
-					where: { 
-						id: req.params.id
-					},
-					include: [Loan]
-				}).then( (book) => {
-					res.render('book_detail',{book});
-					//res.send(book);
-				});
-			});
+	router.get('/:id', (req, res) => {
+		Book.find({
+			where: { 
+				id: req.params.id
+			},
+			include: [
+				{
+					model: Loan,
+					include: [Patron]
+				}
+			]
+		}).then( (book) => {
+			res.render('book_detail',{book});
+			//res.send(book);
+		});
+	});
 
-		// POST
-			router.post('/new', (req, res) => {
-				Book.create(req.body).then((book) => {
-					res.redirect('/');
-				}).catch((error) => {
-					if( error.name === 'SequelizeValidationError' ){
-						res.render('new_book',{errors:error.errors});
-					} else {
-						console.error(error);
-					}
-				});
-			});
+// POST
+	router.post('/new', (req, res) => {
+		Book.create(req.body).then((book) => {
+			res.redirect('/');
+		}).catch((error) => {
+			if( error.name === 'SequelizeValidationError' ){
+				res.render('new_book',{errors:error.errors});
+			} else {
+				console.error(error);
+			}
+		});
+	});
 module.exports = router;
